33baef9280dea650b2d544906a2353eb2efc43e5
saurabh jain <luckyjain0107@gmai.com>
2022-07-07 22:57:08 +0530

    Employee wages

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,15 +2,17 @@ package com.bridgelabz;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage");
-     //   int employeepresent = Math.random();
-        double randomVal = Math.random();
-        double floorVal = Math.floor(randomVal*10);
-        int employeePresent =(int)Math.floor(Math.random()*10) % 2;
-        System.out.println(employeePresent);
-        if(employeePresent == 1)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is absent");
+    int IS_FULL_TIME = 1;
+    int EMP_RATE_PER_HOUR = 20;
+    int empHrs = 0;
+    int empWage =0;
+    double empcheck = Math.floor(Math.random( )* 10) % 2;
+        if (empcheck == IS_FULL_TIME )
+            empHrs = 8;
+    else
+        empHrs =0;
+    empWage =empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: " + empWage);
+
     }
 }
