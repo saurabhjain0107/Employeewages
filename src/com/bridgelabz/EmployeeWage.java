32903e81476041d35c12a276f967fdce5d1b692f
saurabh jain <luckyjain0107@gmai.com>
2022-07-08 21:38:03 +0530

    Emp_Part_time wages

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,16 +1,25 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    static final int IS_FULL_TIME=1;
+    static final int IS_PART_TIME=2;
+    static final int WAGE_PER_HR=20;
+    static final int FULL_DAY_HR=16;
+    static final int PART_TIME_HR=8;
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage");
-     //   int employeepresent = Math.random();
-        double randomVal = Math.random();
-        double floorVal = Math.floor(randomVal*10);
-        int employeePresent =(int)Math.floor(Math.random()*10) % 2;
-        System.out.println(employeePresent);
-        if(employeePresent == 1)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is absent");
+        System.out.println("Welcome to Employee Wage !");
+        double empCheck=Math.floor(Math.random()*10) % 3;
+        int dailywage=0;
+        if(empCheck==IS_FULL_TIME) {
+            System.out.println("employee is full time");
+            dailywage=WAGE_PER_HR * FULL_DAY_HR;
+        } else if (empCheck==IS_PART_TIME) {
+            System.out.println("employee is part time");
+            dailywage=WAGE_PER_HR * PART_TIME_HR;
+        }
+        else {
+            System.out.println("employee is absent");
+        }
+        System.out.println("Daily Wage is "+dailywage);
     }
 }
